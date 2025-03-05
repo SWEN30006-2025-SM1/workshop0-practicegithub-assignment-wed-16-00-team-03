1f0659ef45718a32b67bb147ded80c8599ff7d56
Grace Yang <hanmingyang@Graces-MacBook-Pro.local>
2025-03-05 17:22:23 +1100

    activate workflow

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
         return a+b;
+        // lets see if it works
     }
 
     public int minus(int a, int b) {
